f17d00446de5c2af63132fbee8d1178cd7acdd50
rabindraregmi <regmirabindra2014@gmail.com>
2021-12-16 10:06:32 +0545

    Resolver added for Blog Post

--- src/graphql/resolver/resolver.ts
+++ src/graphql/resolver/resolver.ts
@@ -1,5 +1,5 @@
 import { ValidationError } from "apollo-server";
-import { CategoryModel } from "../../db/models";
+import { BlogModel, CategoryModel } from "../../db/models";
 import { Resolvers } from "../../generated/types";
 
 const resolvers: Resolvers = {
@@ -15,6 +15,55 @@ const resolvers: Resolvers = {
         });
         await newCategory.save();
         return newCategory;
+      } catch (error) {
+        throw new ValidationError(error as any);
+      }
+    },
+    createBlogPost: async (_, { blog }) => {
+      const { title, subtitle, content, published, category } = blog;
+      try {
+        const newBlog = new BlogModel({
+          title,
+          subtitle,
+          content,
+          published,
+          created_at: Date.now(),
+          updated_at: Date.now(),
+        });
+        await newBlog.save();
+        return newBlog;
+      } catch (error) {
+        throw new ValidationError(error as any);
+      }
+    },
+    editBlogPost: async (_, { blog }) => {
+      const { id, title, subtitle, content, published, category } = blog;
+      const _blog = await BlogModel.findOne({ id });
+
+      if (!_blog) throw new Error("Blog Doesn't Exist");
+
+      try {
+        const newBlog = BlogModel.findByIdAndUpdate(id, {
+          title,
+          subtitle,
+          content,
+          published,
+          created_at: Date.now(),
+          updated_at: Date.now(),
+        });
+
+        return newBlog;
+      } catch (error) {
+        throw new ValidationError(error as any);
+      }
+    },
+    deleteBlogPost: async (_, { id }) => {
+      const _blog = await BlogModel.findOne({ id });
+
+      if (!_blog) throw new Error("Blog doesn't exist");
+      try {
+        const deletedBlog = BlogModel.findByIdAndDelete(id);
+        return deletedBlog;
       } catch (error) {
         throw new ValidationError(error as any);
       }
